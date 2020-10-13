2ce88e978f3a3f50a90728450513d62d152013df
Noah Pack <noahpack@noahs-mbp.lan>
2020-10-13 15:07:33 -0600

    Finished the first afternoon project

---
+++ Part2/index.js
@@ -0,0 +1,11 @@
+const idInput = document.getElementById ('idInput')
+const colorInput = document.getElementById ('colorInput')
+
+console.log(idInput);
+console.log(colorInput);
+
+function setCard() {
+    let card = document.getElementById(idInput.value)
+    console.log(card)
+    card.style.color = colorInput.value
+}
